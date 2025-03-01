699d4f8852113b102ce074f8433382a0d0203901
Chazelam <andreilaymzin@gmail.com>
2025-03-01 21:14:07 +0300

    add l6

---
+++ l5 copy/script/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $(".bxslider").bxSlider({
+    pager: false,
+    // captions: true,
+    auto: true,
+  });
+});
+
+$(document).ready(function () {
+  $(".bxslider_team").bxSlider({
+    // pager: false,
+    // captions: true,
+    auto: true,
+    controls: false,
+  });
+});
